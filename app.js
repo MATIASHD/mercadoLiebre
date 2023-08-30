a055d323d755d877479c74c1b0036cc9411c9c25
Matias Arevalo <arevalo.matias418@gmail.com>
2023-08-30 19:31:09 -0300

    Ver que se modificos

--- app.js
+++ app.js
@@ -13,3 +13,10 @@ app.use(express.static(path.resolve(__dirname,'./public')));
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,'./views/home.html'));
 })
+
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname,'./views/register.html'));
+})
+app.get('/signin', (req,res) =>{
+    res.sendFile(path.join(__dirname,'./views/signin.html'));
+})
